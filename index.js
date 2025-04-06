fea339a7c59067042e27ff5d5f3601f00e247093
pedrochatelain <pedrochatelain@gmail.com>
2025-04-06 19:04:36 -0300

    add mock route for adding products

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/api/products', (req, res) => {
+  const product = req.body;
+  console.log('Received product:', product);
+  res.status(200).json({ message: 'Product added successfully', product });
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
