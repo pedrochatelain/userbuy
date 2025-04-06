fd9616c6e5596d7215a554879e6349d3b5b5c48e
pedrochatelain <pedrochatelain@gmail.com>
2025-04-06 18:44:24 -0300

    install express and add basic route

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
