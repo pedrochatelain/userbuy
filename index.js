439c2a538a261a90fea074205cf1ea3e511c2ac1
pedrochatelain <pedrochatelain@gmail.com>
2025-04-06 19:12:17 -0300

    validate product before adding

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const validateProduct = require('./middlewares/productValidator');
+const { validationResult } = require('express-validator');
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -10,7 +12,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/api/products', (req, res) => {
+app.post('/api/products', validateProduct, (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
   const product = req.body;
   console.log('Received product:', product);
   res.status(200).json({ message: 'Product added successfully', product });
