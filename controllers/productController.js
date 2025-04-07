792e63762e33636b6a604c1841aa1e417ce923cc
pedrochatelain <pedrochatelain@gmail.com>
2025-04-07 20:18:25 -0300

    create controller addProduct

---
+++ controllers/productController.js
@@ -0,0 +1,23 @@
+const { validationResult } = require('express-validator');
+const service = require('../services/productServices'); // Adjust the path as needed
+
+const addProduct = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const product = req.body;
+  console.log('Received product:', product);
+
+  try {
+    service.addProduct(product);
+    res.status(200).json({ message: 'Product added successfully', product });
+  } catch (error) {
+    console.error('Error adding product:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+module.exports = { addProduct };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,22 +2,11 @@ const express = require('express');
 const router = express.Router();
 const validateProduct = require('../middlewares/productValidator');
 const { validateObjectId } = require('../middlewares/objectIdValidator')
-const { validationResult } = require('express-validator');
+const productController = require('../controllers/productController')
 const service = require('../services/productServices')
 
 // POST /api/products
-router.post('/api/products', validateProduct, (req, res) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const product = req.body;
-  console.log('Received product:', product);
-  service.addProduct(product)
-  res.status(200).json({ message: 'Product added successfully', product });
-});
+router.post('/api/products', validateProduct, productController.addProduct);
 
 // GET /api/products
 router.get('/api/products', async (req, res) => {
