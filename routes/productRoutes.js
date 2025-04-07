fb8edf3acb0647d318700263e77253fefcfba06a
pedrochatelain <pedrochatelain@gmail.com>
2025-04-07 13:02:58 -0300

    use service on productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const validateProduct = require('../middlewares/productValidator');
 const { validationResult } = require('express-validator');
+const service = require('../services/productServices')
 
 // POST /api/products
 router.post('/', validateProduct, (req, res) => {
@@ -13,6 +14,7 @@ router.post('/', validateProduct, (req, res) => {
 
   const product = req.body;
   console.log('Received product:', product);
+  service.addProduct(product)
   res.status(200).json({ message: 'Product added successfully', product });
 });
 
