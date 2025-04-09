8024b26c8fa47b693493d9ce502e02b69b00f3aa
pedrochatelain <pedrochatelain@gmail.com>
2025-04-09 13:06:12 -0300

    hash user password before persisting to db

--- services/userService.js
+++ services/userService.js
@@ -1,10 +1,28 @@
 const { getUsersCollection } = require('../datasources/mongo')
+const bcrypt = require('bcrypt');
 
 async function createUser(user) {
-    const usersCollection = getUsersCollection()
-    const result = await usersCollection.insertOne(user);
-    // Check if the document exists in the database
-    return await usersCollection.findOne({ _id: result.insertedId });
+    try {
+        user.password = await hashPassword(user.password)
+        const usersCollection = getUsersCollection()
+        const result = await usersCollection.insertOne(user);
+        // Check if the document exists in the database
+        return await usersCollection.findOne({ _id: result.insertedId });
+
+    } catch(error) {
+        return null
+    }
+}
+
+async function hashPassword(password) {
+    const saltRounds = 10; // Adjust as needed
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        return hashedPassword;
+    } catch (error) {
+        console.error("Error hashing password:", error);
+        throw error;
+    }
 }
 
 module.exports = { createUser }
