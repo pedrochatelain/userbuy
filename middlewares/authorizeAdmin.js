c52b9d09f19116f4d0ec41cff745686b82172414
pedrochatelain <pedrochatelain@gmail.com>
2025-04-10 10:24:54 -0300

    add middleware to check if user is admin

---
+++ middlewares/authorizeAdmin.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1]; // Assuming token is in 'Bearer <token>' format
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access token is missing' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Replace with your JWT secret
+    if (decoded.rol !== 'admin') {
+      return res.status(403).json({ message: 'Forbidden: Admin access required' });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid or expired token' });
+  }
+};
