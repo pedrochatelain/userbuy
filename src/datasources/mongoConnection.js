ae88d8a126f1e248ae11b0bcd64efaa76ef86fef
pedrochatelain <pedrochatelain@gmail.com>
2025-04-20 23:35:37 -0300

    remove options argument

--- src/datasources/mongoConnection.js
+++ src/datasources/mongoConnection.js
@@ -3,10 +3,7 @@ const { MongoClient } = require('mongodb');
 let dbInstance = null;
 
 async function connectDB() {
-  const client = new MongoClient(process.env.MONGO_URI, {
-    ssl: true, // Explicitly enable SSL
-    tlsAllowInvalidCertificates: false, // Ensure TLS validation
-  });
+  const client = new MongoClient(process.env.MONGO_URI);
   await client.connect();
   dbInstance = client.db('mydatabase');
   console.log(`Connected to MongoDB: mydatabase`);
