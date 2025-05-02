a1f75e7a00682d3e03ccc33d48d7a500fb065e9a
pedrochatelain <pedrochatelain@gmail.com>
2025-05-02 13:03:54 -0300

    allow only logged in user to update balances

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController')
 const validateUser = require('../middlewares/validateUser')
 const validateRoleUpdate = require('../middlewares/validateRoleUpdate');
 const handleValidationErrors = require('../middlewares/handleValidationErrors');
+const authorizeUser = require('../middlewares/authorizeUser')
 
 // POST /api/users
 router.post('/api/users', validateUser, userController.createUser);
@@ -15,6 +16,6 @@ router.get('/api/users', userController.getUsers);
 router.patch('/api/users/:userId/roles', validateRoleUpdate, handleValidationErrors, userController.editRoles)
 
 // PATCH /api/users/:userId/balances
-router.patch('/api/users/:userId/balances', userController.addToBalances)
+router.patch('/api/users/:userId/balances', authorizeUser, userController.addToBalances)
 
 module.exports = router;
