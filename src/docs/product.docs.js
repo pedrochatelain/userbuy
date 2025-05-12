64b171a45d21cd7da49c75564310aded7ceec32f
pedrochatelain <pedrochatelain@gmail.com>
2025-05-12 15:26:35 -0300

    add swagger to `PUT: api/products`

--- src/docs/product.docs.js
+++ src/docs/product.docs.js
@@ -6,6 +6,45 @@ const productPaths = {
   },
   components: {
     responses: {
+      Unauthorized: {
+        description: "Error: Unauthorized",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                message: { type: "string" },
+              },
+            },
+            examples: {
+              missingToken: {
+                summary: "Missing Token",
+                value: { message: "Access token is missing" },
+              },
+              invalidToken: {
+                summary: "Invalid or Expired Token",
+                value: { message: "Invalid or expired token" },
+              },
+            },
+          },
+        },
+      },
+      Forbidden: {
+        description: "Error: Forbidden",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                error: {
+                  type: "string",
+                  example: "Forbidden: You do not have permission to perform this action",
+                },
+              },
+            },
+          },
+        },
+      },
       BadRequest: {
         description: "Error: Bad Request",
         content: {
@@ -144,6 +183,69 @@ const productPaths = {
         },
       },
     },
+    "/api/products/{idProduct}": {
+      put: {
+        summary: "Update a product",
+        tags: ["Products"],
+        parameters: [
+          {
+            name: "idProduct",
+            in: "path",
+            required: true,
+            schema: { type: "string" },
+            description: "ID of the product",
+          },
+        ],
+        requestBody: {
+          required: true,
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  category: { type: "string", example: "categ" },
+                  name: { type: "string", example: "name" },
+                  stock_quantity: { type: "integer", example: 8 },
+                  price: { type: "number", example: 1023.45 },
+                  currency: { type: "string", example: "USD" },
+                  image: { type: "string", example: "http://www.images.com/yourimage" },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description: "Product updated successfully",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    message: { type: "string", example: "Product updated successfully" },
+                    product: {
+                      type: "object",
+                      properties: {
+                        category: { type: "string", example: "categ" },
+                        name: { type: "string", example: "name" },
+                        stock_quantity: { type: "integer", example: 8 },
+                        price: { type: "number", example: 1023.45 },
+                        currency: { type: "string", example: "USD" },
+                        image: { type: "string", example: "http://www.images.com/yourimage" },
+                        _id: { type: "string", example: "681fd1dae07055db24b902a9" },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+          400: { $ref: "#/components/responses/BadRequest" },
+          401: { $ref: "#/components/responses/Unauthorized" },
+          403: { $ref: "#/components/responses/Forbidden" },
+        },
+      }
+    }
   },
 };
 
