9016accff87643430dfcb10345315690c216d300
pedrochatelain <pedrochatelain@gmail.com>
2025-05-23 21:11:00 -0300

    clean up index file

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const { connectDB } = require('./src/config/database.mongodb.js');
 const cors = require('cors');
+const { connectDB } = require('./src/config/database.mongodb');
 const { registerRoutes } = require('./src/config/routes');
+const { syntaxErrorHandler } = require('./src/middlewares/handleSyntaxError');
 const { setupSwagger } = require('./src/config/swagger');
-const { syntaxErrorHandler } = require('./src/middlewares/handleSyntaxError.js');
 
+const app = express();
 
-// Swagger setup
-setupSwagger(app)
-
-// Middleware to parse JSON
+// Middleware
 app.use(express.json());
+app.use(cors());
+
+// Setup Swagger
+setupSwagger(app);
+
+// Register Routes
+registerRoutes(app);
 
 // Error-handling middleware
 app.use(syntaxErrorHandler);
 
-// Enable CORS
-app.use(cors());
-
-registerRoutes(app)
-
-require('dotenv').config();
-
-
-async function connectToDatabase() {
+// Connect to Database and Start Server
+async function startServer() {
   try {
     await connectDB();
     console.log('Database initialized successfully');
-    // Add your server initialization code here
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    });
   } catch (error) {
     console.error('Failed to connect to the database:', error);
     process.exit(1);
   }
 }
 
-connectToDatabase();
-
-// Start server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
-});
+startServer();
