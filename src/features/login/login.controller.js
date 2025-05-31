afef1b0d7c3536e75e9a2b14dc454de852af5c97
pedrochatelain <pedrochatelain@gmail.com>
2025-05-31 20:03:40 -0300

    verify token before adding it to blacklist

--- src/features/login/login.controller.js
+++ src/features/login/login.controller.js
@@ -1,4 +1,5 @@
 const service = require('./login.service');
+const { verifyToken } = require('../../utils/auth.utils')
 
 const login = async (req, res) => {
     try {
@@ -11,13 +12,13 @@ const login = async (req, res) => {
 
 const logout = async (req, res) => {
     try {
+        verifyToken(req)
         const blacklistedToken = await service.logout(req.body.token)
         res.status(200).json({ message: "User logged out successfully", blacklistedToken})            
     } catch (err) {
         if ( ! err.statusCode)
             err.statusCode = 500
-        console.log(err)
-        res.status(err.statusCode).json(err)
+        res.status(err.statusCode).json({err, message: err.message})
     }
 }
 
